6027fa6f4765f2daca28e1db8624f60fcb3154f7
allwayz <2584491610@qq.com>
2020-11-06 02:26:21 +0000

    Server parameter verification

--- malle-product/src/main/java/cn/allwayz/product/MalleProductApplication.java
+++ malle-product/src/main/java/cn/allwayz/product/MalleProductApplication.java
@@ -1,6 +1,5 @@
 package cn.allwayz.product;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author allwayz
  */
 @EnableDiscoveryClient
-@MapperScan("cn.allwayz.product.dao")
 @SpringBootApplication
 public class MalleProductApplication {
 
